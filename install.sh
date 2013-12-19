32e0142bf92a366a2fd713a67148014f5b3bb1c1
Alexis Simonnet <lekhder.aissam@gmail.com>
2013-12-19 16:37:18 +0100

    ajout des apt-get pour les lib png et fftw

--- install.sh
+++ install.sh
@@ -8,18 +8,19 @@ sudo ln -s /usr/bin/g++-4.4 /usr/bin/g++
 sudo apt-get install gcc-4.4
 sudo ln -s /usr/bin/gcc-4.4 /usr/bin/gcc
 sudo apt-get install python
-sudo apt-get install libgsl0
-sudo apt-get install libgsl0-devel
-sudo apt-get install libxml2-python
+sudo apt-get install python-dev
+sudo apt-get install libgsl0-dev
 sudo apt-get install libxml2-dev
-sudo apt-get install libxml++2.6
+sudo apt-get install libxml++2
 sudo apt-get install libpython-devel
-sudo apt-get install swig-1.3.21
+sudo apt-get install swig
 sudo apt-get install libboost-dev
 sudo apt-get install python-all-dev
-sudo apt-get install libg2c
+#sudo apt-get install libg2c            #Trouver une autre source
 sudo apt-get install openmpi-bin
 sudo apt-get install libopenmpi-dev
+sudo apt-get install libpng-dev
+sudo apt-get install libfftw3-dev
 
 # Pour Metil
 sudo apt-get install libreadline-dev
@@ -27,7 +28,7 @@ sudo apt-get install cmake
 sudo apt-get install libqt4-core
 sudo apt-get install qt4-qmake
 sudo apt-get install libhdf5-serial-dev
-sudo apt-get install qt
+#sudo apt-get install qt                #Pas de paquet correspondant
 sudo apt-get install qtcreator
 
 # Pour la visu
@@ -41,5 +42,5 @@ sudo apt-get install coffeescript
 # Pour cuda
 sudo apt-get install libxi-dev
 sudo apt-get install libxmu-dev
-sudo apt-get install libglut3-dev
+sudo apt-get install freeglut3-dev
 
