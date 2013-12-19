483745f06b6351ac08f7a1a684c446b6256ecf6c
Alexis Simonnet <lekhder.aissam@gmail.com>
2013-12-19 14:15:04 +0100

    ajout d'un bash d'installation

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+#Pour les codes
+sudo apt-get install kdevelop
+sudo apt-get install scons
+sudo apt-get install g++-4.4
+sudo ln -s /usr/bin/g++-4.4 /usr/bin/g++
+sudo apt-get install gcc-4.4
+sudo ln -s /usr/bin/gcc-4.4 /usr/bin/gcc
+sudo apt-get install python
+sudo apt-get install libgsl0
+sudo apt-get install libgsl0-devel
+sudo apt-get install libxml2-python
+sudo apt-get install libxml2-dev
+sudo apt-get install libxml++2.6
+sudo apt-get install libpython-devel
+sudo apt-get install swig-1.3.21
+sudo apt-get install libboost-dev
+sudo apt-get install python-all-dev
+sudo apt-get install libg2c
+sudo apt-get install openmpi-bin
+sudo apt-get install libopenmpi-dev
+
+# Pour Metil
+sudo apt-get install libreadline-dev
+sudo apt-get install cmake
+sudo apt-get install libqt4-core
+sudo apt-get install qt4-qmake
+sudo apt-get install libhdf5-serial-dev
+sudo apt-get install qt
+sudo apt-get install qtcreator
+
+# Pour la visu
+sudo apt-get install zlib1g-dev
+sudo apt-get install libglew-dev
+sudo apt-get install libfcgi-dev
+
+# Pour l'interface
+sudo apt-get install coffeescript
+
+# Pour cuda
+sudo apt-get install libxi-dev
+sudo apt-get install libxmu-dev
+sudo apt-get install libglut3-dev
+
